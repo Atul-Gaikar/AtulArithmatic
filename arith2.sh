dd61396fe6dbd57d27d6bf6b7bb99a744ae686a1
Atul <atulgaikarag9@gmail.com>
2021-05-22 11:19:48 +0530

    Adding file UC2

--- arith2.sh
+++ arith2.sh
@@ -1,3 +1,6 @@
 #! /bin/bash
+read -p "Enter the number1"number1
+read -p "Enter the number2"number2
+read -p "Enter the number3"number3
 arithmatic1=$(( number1+number2*number3 ))
 echo "arithmatic1" $arithmatic1
